75264611414119a5f57011049e0fceb4b5ddf410
Maxwell Githinji <muriumaxwell1@gmail.com>
2025-03-07 23:36:53 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName= "bob";
+function upperCaseCustomerName(){
+    customerName = customerName.toLocaleUpperCase();
+}
+function setBestCustomer(){
+     bestCustomer = "not bob";
+    console.log(bestCustomer);
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    console.log(bestCustomer)
+}
+const leastFavoriteCustomer = "MAxwell";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "bob";
+    console.log (leastFavoriteCustomer);
+}
